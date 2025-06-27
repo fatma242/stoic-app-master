6bd4d7f4ea151adea08718373d0d105882795cea
ahmedadelelshaer <138902289+ahmedadelelshaer@users.noreply.github.com>
2025-06-27 19:21:33 +0300

    merge main with Adel

--- Back/src/main/java/com/example/stoic/Comment/Model/Comment.java
+++ Back/src/main/java/com/example/stoic/Comment/Model/Comment.java
@@ -3,7 +3,6 @@ package com.example.stoic.Comment.Model;
 import com.example.stoic.Post.Model.Post;
 
 import com.example.stoic.User.Model.User;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.*;
@@ -12,9 +11,6 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Data
 @NoArgsConstructor
@@ -37,15 +33,13 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User author;
+
     @ManyToMany
-    @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinTable(name = "comment_likes", joinColumns = @JoinColumn(name = "comment_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> likes;
 
-    @ManyToOne( fetch = FetchType.LAZY)
-    @JsonIgnore // This prevents Post → Comment → Post infinite loop
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
     private Post post;
 
     @Column(name = "no_reports")
